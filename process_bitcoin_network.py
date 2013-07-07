9b4e155f942c3926c3b9bf1546cd30513211e045
Barret Schloerke <schloerke@gmail.com>
2013-07-07 17:58:12 -0400

    added "start" print statements

--- process_bitcoin_network.py
+++ process_bitcoin_network.py
@@ -18,17 +18,21 @@ edges_out                = sys.argv[7]
 db_path                  = sys.argv[8]
 
 #index and write out long key strings, use numeric data instead (scalability consideration). Also build main data rows.
+print "Starting: writing network dictionaries"
 rows = write_network_dictionaries(db_path, transactions_in, transaction_keys, pub_keys)
 print "Completed: writing network dictionaries"
 
 #preprocessing: input pubkeys from 'coinbase' are sometimes given in raw data as (None), these can be fixed by using the output pubkey
+print "Starting: fixing missing public keys"
 rows = fix_missing_public_keys(rows)
 print "Completed: fixing missing public keys"
 
 #group public keys used as shared inputs to a transaction (i.e. a "user" owns the private key to each address).
+print "Starting: writing user public keys"
 user_hash = write_user_public_keys(users_out, rows)
 print "Completed: writing user public keys"
 
 #write main data file
+print "Starting: writing user edges"
 write_user_edges(referents_out, referent_public_keys_out, edges_out, rows, user_hash)
 print "Completed: writing user edges"
